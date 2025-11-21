b10aa2684d2a6e3f975864490cfeb2c327ebc3ef
VandexClient <bloonseban@gmail.com>
2025-11-22 04:49:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import threading
 import time
 from datetime import datetime
 import os
-import eventlet
+from gevent import monkey
+monkey.patch_all()
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
